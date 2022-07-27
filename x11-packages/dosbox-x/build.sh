571328019715a5b9e04939c72486c954f45b4e69
Jia Yuan Lo <jylo06g@gmail.com>
2022-07-27 22:51:04 +0800

    test: vanilla

--- x11-packages/dosbox-x/build.sh
+++ x11-packages/dosbox-x/build.sh
@@ -6,13 +6,10 @@ TERMUX_PKG_VERSION=0.84.0
 TERMUX_PKG_SRCURL=https://github.com/joncampbell123/dosbox-x/archive/refs/tags/dosbox-x-v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=564fbf8f0ab090c8b32bc38637c8204358c386b9cbffcb4f99a81bc82fddbad7
 TERMUX_PKG_DEPENDS="libc++, sse2neon, libpng, libiconv, libx11, sdl2, sdl2-net, zlib"
-TERMUX_PKG_BLACKLISTED_ARCHES="i686"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-avcodec
 --enable-sdl2
---disable-dynamic-x86
---disable-fpu-x86
 --disable-opengl
 "
 
