d08fde91bd00c64311d5035e660dced6055a224d
Jia Yuan Lo <jylo06g@gmail.com>
2022-08-31 21:55:47 +0800

    suggested changes

--- packages/bcal/build.sh
+++ packages/bcal/build.sh
@@ -6,6 +6,7 @@ TERMUX_PKG_VERSION="2.4"
 TERMUX_PKG_SRCURL=https://github.com/jarun/bcal/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=141f39d866f62274b2262164baaac6202f60749862c84c2e6ed231f6d03ee8df
 TERMUX_PKG_DEPENDS="bc, readline"
+TERMUX_PKG_BUILD_IN_SRC=true
 
 # https://github.com/jarun/bcal/issues/4
 TERMUX_PKG_BLACKLISTED_ARCHES="arm, i686"
