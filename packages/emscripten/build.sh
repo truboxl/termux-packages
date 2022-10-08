ae9260923a765eb218bb528d56972f22267531e6
Jia Yuan Lo <jylo06g@gmail.com>
2022-10-09 07:07:19 +0800

    2f3d7c2cc770c902b3a2fabc2ea92b98bf8f3e0a

--- packages/emscripten/build.sh
+++ packages/emscripten/build.sh
@@ -53,7 +53,7 @@ opt/emscripten/LICENSE
 
 # https://github.com/emscripten-core/emscripten/issues/11362
 # can switch to stable LLVM to save space once above is fixed
-_LLVM_COMMIT=0ef404a9f993047adbbd11a6c98114eec08c75bd
+_LLVM_COMMIT=2f3d7c2cc770c902b3a2fabc2ea92b98bf8f3e0a
 #_LLVM_TGZ_SHA256=26d6d4ba7844de81ae600d6f804fb047809079925072a557bc3faf4a5d4b0b48
 
 # https://github.com/emscripten-core/emscripten/issues/12252
