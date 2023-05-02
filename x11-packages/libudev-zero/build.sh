7487ee357a3db2bc98c1448201d9c681196469de
Jia Yuan Lo <jylo06g@gmail.com>
2023-05-02 22:28:40 +0800

    f

---
+++ x11-packages/libudev-zero/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/illiliti/libudev-zero
+TERMUX_PKG_DESCRIPTION="Daemonless replacement for libudev"
+TERMUX_PKG_LICENSE="ISC"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=1.0.2
+TERMUX_PKG_SRCURL=https://github.com/illiliti/libudev-zero/archive/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=29dff942cab9519994fb92ba6407f57e08d3dd6e6c0b86bb93d7b1d681994ff8
+TERMUX_PKG_NO_STATIC_SPLIT=true
