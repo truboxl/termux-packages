752028be281b404efdb58fc65c4f2481a1d5f19b
Jia Yuan Lo <jylo06g@gmail.com>
2023-06-18 06:48:18 +0800

    fastfetch: fix deps
    Doesnt use freetype and vulkan at all

--- packages/fastfetch/build.sh
+++ packages/fastfetch/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="A neofetch-like tool for fetching system information and
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.11.3"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/LinusDierheimer/fastfetch/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=699ee59b0677121411a11119a06bdb5bc45dc04b551904c9d9d30477e2142358
-TERMUX_PKG_DEPENDS="vulkan-headers, vulkan-loader-android, freetype"
 TERMUX_PKG_AUTO_UPDATE=true
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
