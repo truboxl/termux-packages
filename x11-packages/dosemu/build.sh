45fd84baef8b314ca0354fee6265c74616944731
Jia Yuan Lo <jylo06g@gmail.com>
2023-09-17 01:19:44 +0800

    f

--- x11-packages/dosemu/build.sh
+++ x11-packages/dosemu/build.sh
@@ -6,7 +6,7 @@ _COMMIT=a2ffb6b3ce856c53c2705646dd19537e6894e8ea
 TERMUX_PKG_VERSION=1.4.0.8
 TERMUX_PKG_SRCURL=https://sourceforge.net/code-snapshots/git/d/do/dosemu/code.git/dosemu-code-${_COMMIT}.zip
 TERMUX_PKG_SHA256=16a0e18bcea53d0959716e1f598e881d5697a0ea86e54084bb052e5ae5cc56f7
-UTERMUX_PKG_DEPENDS="libc++, libpng, libx11, sdl, sdl-net, zlib"
+TERMUX_PKG_DEPENDS="libx11, sdl"
 
 UTERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-dynamic-x86
@@ -15,5 +15,6 @@ UTERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 "
 
 termux_step_pre_configure() {
+	[[ "${TERMUX_ARCH}" == "i686" ]] && CFLAGS+=" -fPIC"
 	autoreconf -fi
 }
