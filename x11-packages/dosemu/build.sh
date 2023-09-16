17f28953c88fcdf99645ec5a23b69e536e2d88fc
Jia Yuan Lo <jylo06g@gmail.com>
2023-09-17 00:11:52 +0800

    addpkg(main): dosemu

---
+++ x11-packages/dosemu/build.sh
@@ -0,0 +1,15 @@
+TERMUX_PKG_HOMEPAGE=https://sourceforge.net/projects/dosemu/
+TERMUX_PKG_DESCRIPTION="DOS Emulator"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+_COMMIT=a2ffb6b3ce856c53c2705646dd19537e6894e8ea
+TERMUX_PKG_VERSION=1.4.0.8
+TERMUX_PKG_SRCURL=https://sourceforge.net/code-snapshots/git/d/do/dosemu/code.git/dosemu-code-${_COMMIT}.zip
+TERMUX_PKG_SHA256=16a0e18bcea53d0959716e1f598e881d5697a0ea86e54084bb052e5ae5cc56f7
+UTERMUX_PKG_DEPENDS="libc++, libpng, libx11, sdl, sdl-net, zlib"
+
+UTERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+--disable-dynamic-x86
+--disable-fpu-x86
+--disable-opengl
+"
