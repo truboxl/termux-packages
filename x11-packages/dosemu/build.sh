c4e370e1f363d97e99ed84769fbc969933fe211d
Jia Yuan Lo <jylo06g@gmail.com>
2023-09-17 12:14:24 +0800

    f

--- x11-packages/dosemu/build.sh
+++ x11-packages/dosemu/build.sh
@@ -7,8 +7,9 @@ TERMUX_PKG_SRCURL=git+https://git.code.sf.net/p/dosemu/code
 TERMUX_PKG_GIT_BRANCH=dosemu-${TERMUX_PKG_VERSION}
 UTERMUX_PKG_SHA256=
 TERMUX_PKG_DEPENDS="libandroid-shmem, libx11, sdl"
-TERMUX_PKG_BUILD_DEPENDS="binutils-cross"
+UTERMUX_PKG_BUILD_DEPENDS="binutils-cross"
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_BLACKLISTED_ARCHES="aarch64, arm"
 UTERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-dynamic-x86
 --disable-fpu-x86
@@ -16,10 +17,10 @@ UTERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 "
 
 termux_step_pre_configure() {
-	termux_setup_no_integrated_as
+	#termux_setup_no_integrated_as
 	CFLAGS+=" -fno-integrated-as"
 	LDFLAGS+=" -landroid-shmem"
-	[[ "${TERMUX_ARCH}" == "i686" ]] && CFLAGS+=" -fPIC"
+	[[ "${TERMUX_ARCH}" == "i686" ]] && ASFLAGS+=" -fPIC"
 	autoreconf -fi
 	pushd src/plugin/sdl
 	autoreconf -fi
