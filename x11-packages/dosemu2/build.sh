adfc23f7702266c7e735c2f47955ec07a62cc3d6
Jia Yuan Lo <jylo06g@gmail.com>
2023-09-24 23:54:18 +0800

    f

--- x11-packages/dosemu2/build.sh
+++ x11-packages/dosemu2/build.sh
@@ -5,18 +5,15 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.0-b7076
 TERMUX_PKG_SRCURL=git+https://github.com/dosemu2/dosemu2
 TERMUX_PKG_GIT_BRANCH=2.0pre9
-TERMUX_PKG_DEPENDS="libandroid-shmem, libx11, sdl"
+UTERMUX_PKG_DEPENDS="libx11"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
 	#termux_setup_no_integrated_as
 	#CFLAGS+=" -fno-integrated-as"
-	LDFLAGS+=" -landroid-shmem -Wl,-pie"
-	export ASFLAGS+=" -fPIC"
-	autoreconf -fi
-	pushd src/plugin/sdl
-	autoreconf -fi
-	popd
+	#LDFLAGS+=" -landroid-shmem"
+	#export ASFLAGS+=" -fPIC"
+	./autogen.sh
 }
 
 # dosemu2 versioning is a mess
