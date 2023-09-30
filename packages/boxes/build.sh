c61f19680a747ff287b75601c625a41f979747d4
Jia Yuan Lo <jylo06g@gmail.com>
2023-09-30 16:17:15 +0800

    f

--- packages/boxes/build.sh
+++ packages/boxes/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="A command line filter program which draws ASCII art boxe
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.2.1
-TERMUX_PKG_SRCURL=https://github.com/ascii-boxes/boxes/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SRCURL=https://github.com/ascii-boxes/boxes/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=f29854f50734ba258788122cfbaa50dd01c040252735bb8c2ba6a236de09e822
 TERMUX_PKG_DEPENDS="libunistring, pcre2"
 TERMUX_PKG_BUILD_IN_SRC=true
@@ -24,3 +24,7 @@ termux_step_make_install() {
 	install -Dm600 -t $TERMUX_PREFIX/share/man/man1 doc/boxes.1
 	install -Dm600 -t $TERMUX_PREFIX/share/boxes boxes-config
 }
+
+# Do not use https://github.com/ascii-boxes/boxes/archive as SRCURL
+# https://github.com/ascii-boxes/boxes/archive/v2.2.1.tar.gz
+# the given path has multiple possibilities: #<Git::Ref:0x00007f3805df9d98>, #<Git::Ref:0x00007f3805df94d8>
