c1da6be4e595926887ed894a23bb5c1cc57864ce
Jia Yuan Lo <jylo06g@gmail.com>
2023-09-30 15:40:43 +0800

    bump(main/boxes): 2.2.1

--- packages/boxes/build.sh
+++ packages/boxes/build.sh
@@ -2,11 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://boxes.thomasjensen.com/
 TERMUX_PKG_DESCRIPTION="A command line filter program which draws ASCII art boxes around your input text"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=2.2.0
+TERMUX_PKG_VERSION=2.2.1
 TERMUX_PKG_SRCURL=https://github.com/ascii-boxes/boxes/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=98b8e3cf5008f46f096d5775d129c34db9f718728bffb0f5d67ae89bb494102e
 TERMUX_PKG_DEPENDS="libunistring, pcre2"
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_CONFFILES="
 share/boxes/boxes-config
 "
