05fdb13b3e4e584503d8b75bc2ad25bfba1f3d27
Jia Yuan Lo <jylo06g@gmail.com>
2023-10-02 00:36:56 +0800

    bump(x11/mate-common): 1.27.1

--- x11-packages/mate-common/build.sh
+++ x11-packages/mate-common/build.sh
@@ -3,11 +3,11 @@ TERMUX_PKG_DESCRIPTION="Common scripts and macros to develop with MATE"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.27.1"
-TERMUX_PKG_SRCURL=https://github.com/mate-desktop/mate-common/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=a1515ad821cf93880a155f58f8c18dc0a071bd099caef900b9f122672197069a
-TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_SRCURL=https://github.com/mate-desktop/mate-common/releases/download/v${TERMUX_PKG_VERSION}/mate-common-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=4f1f91d0d60e3629999e2ff930574882f511a15831e20b52d93ff0bc8922effa
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
+TERMUX_PKG_AUT0_UPDATE=true
 
 termux_step_pre_configure() {
-	autoreconf -fi
+	autoreconf -vfi
 }
