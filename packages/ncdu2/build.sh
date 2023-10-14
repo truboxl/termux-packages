bd04128ab04214ad0b3b834557750bc0edba7509
Jia Yuan Lo <jylo06g@gmail.com>
2023-10-15 01:26:45 +0800

    f

--- packages/ncdu2/build.sh
+++ packages/ncdu2/build.sh
@@ -10,32 +10,18 @@ TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_post_get_source() {
 	# TODO drop all this once figure out how zig can work with bionic libc
-	local NCURSES_SRCURL=$(. "${TERMUX_SCRIPTDIR}"/packages/ncurses/build.sh; echo ${TERMUX_PKG_SRCURL[*]})
-	local NCURSES_SHA256=$(. "${TERMUX_SCRIPTDIR}"/packages/ncurses/build.sh; echo ${TERMUX_PKG_SHA256[*]})
-	local l=$(echo "${NCURSES_SRCURL}" | wc -w)
+	local NCURSES_SRCURL=$(. "${TERMUX_SCRIPTDIR}"/packages/ncurses/build.sh; echo ${TERMUX_PKG_SRCURL[0]})
+	local NCURSES_SHA256=$(. "${TERMUX_SCRIPTDIR}"/packages/ncurses/build.sh; echo ${TERMUX_PKG_SHA256[0]})
 
-	rm -fr external ncurses
-	mkdir -p external
+	rm -fr ncurses-* ncurses
+	termux_download "${NCURSES_SRCURL}" "${TERMUX_PKG_CACHEDIR}/ncurses.tar.gz" "${NCURSES_SHA256}"
+	tar -xf "${TERMUX_PKG_CACHEDIR}/ncurses.tar.gz"
+	mv -v ncurses-* ncurses
 
-	for i in $(seq 1 "${l}"); do
-	local srcurl=$(echo "${NCURSES_SRCURL}" | awk "{print \$${i}}")
-	local sha256=$(echo "${NCURSES_SHA256}" | awk "{print \$${i}}")
-	local tar=${TERMUX_PKG_CACHEDIR}/$(basename "${srcurl}")
-	termux_download "${srcurl}" "${tar}" "${sha256}"
-	tar -xf "${tar}" -C external
-	done
-
-	pushd external
-	for i in $(ls); do
-	mv -v "${i}" "${i//-*}"
-	done
 	echo "INFO: Applying patches from packages/ncurses"
 	for p in "${TERMUX_SCRIPTDIR}"/packages/ncurses/*.patch; do
 	patch -p1 -i "${p}" -d ncurses
 	done
-	popd
-
-	ln -sv external/ncurses ncurses
 
 	sed \
 		-e "s|--with-default-terminfo-dir=/usr|--with-default-terminfo-dir=${TERMUX_PREFIX}|" \
