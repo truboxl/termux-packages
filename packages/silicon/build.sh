afbbab20dbbc2a50ef5c87ffb5a5ffc231641375
Jia Yuan Lo <jylo06g@gmail.com>
2023-12-28 17:38:34 +0800

    bump(main/silicon): 0.5.2

--- packages/silicon/build.sh
+++ packages/silicon/build.sh
@@ -2,17 +2,23 @@ TERMUX_PKG_HOMEPAGE=https://github.com/Aloxaf/silicon
 TERMUX_PKG_DESCRIPTION="Silicon is an alternative to Carbon implemented in Rust"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Yaksh Bariya <thunder-coding@termux.dev>"
-TERMUX_PKG_VERSION=0.5.1
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION="0.5.2"
 TERMUX_PKG_SRCURL=https://github.com/Aloxaf/silicon/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=784a6f99001f2000422b676e637fe83a5dc27f5fc55ad33e227c882ce20e6439
-TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_SHA256=815d41775dd9cd399650addf8056f803f3f57e68438e8b38445ee727a56b4b2d
+TERMUX_PKG_DEPENDS="fontconfig, harfbuzz"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_DEPENDS="fontconfig, freetype, harfbuzz"
+TERMUX_PKG_AUTO_UPDATE=true
 
 termux_step_pre_configure() {
-	termux_setup_rust
 	termux_setup_cmake
+	termux_setup_rust
+
+	local CFLAGS=$(termux_step_setup_toolchain; echo ${CFLAGS})
+	export TARGET_CMAKE_TOOLCHAIN_FILE="${TERMUX_PKG_BUILDDIR}/android.toolchain.cmake"
+	cat <<- EOF > "${TERMUX_PKG_BUILDDIR}/android.toolchain.cmake"
+	set(CMAKE_C_FLAGS "${CFLAGS}")
+	set(CMAKE_CXX_FLAGS "${CXXFLAGS}")
+	EOF
 }
 
 termux_step_make() {
