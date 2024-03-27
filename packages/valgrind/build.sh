c5279b35121f603e83b02333bf67a291e8baa17e
Jia Yuan Lo <jylo06g@gmail.com>
2024-03-27 12:35:15 +0800

    fix(main/valgrind): various fixes
    * Disable syscalls access, open, stat, statx
    * Prefer syscalls faccessat, openat, newfstatat, fstatat64
    * Split to per file patches
    * Add missing amd64_linux_android support
    * Add patch to fix clone support on Android

--- packages/valgrind/build.sh
+++ packages/valgrind/build.sh
@@ -3,12 +3,13 @@ TERMUX_PKG_DESCRIPTION="Instrumentation framework for building dynamic analysis 
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="3.22.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=http://sourceware.org/pub/valgrind/valgrind-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=c811db5add2c5f729944caf47c4e7a65dcaabb9461e472b578765dd7bf6d2d4c
-TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_DEPENDS="binutils-cross"
 TERMUX_PKG_BREAKS="valgrind-dev"
 TERMUX_PKG_REPLACES="valgrind-dev"
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-tmpdir=$TERMUX_PREFIX/tmp"
 
 termux_step_pre_configure() {
@@ -16,8 +17,8 @@ termux_step_pre_configure() {
 
 	if [ "$TERMUX_ARCH" == "aarch64" ]; then
 		cp $TERMUX_PKG_BUILDER_DIR/aarch64-setjmp.S $TERMUX_PKG_SRCDIR
-		patch --silent -p1 < $TERMUX_PKG_BUILDER_DIR/coregrindmake.am.diff
-		patch --silent -p1 < $TERMUX_PKG_BUILDER_DIR/memcheckmake.am.diff
+		patch --silent -p1 < $TERMUX_PKG_BUILDER_DIR/0001-coregrindmake.am.diff
+		patch --silent -p1 < $TERMUX_PKG_BUILDER_DIR/0002-memcheckmake.am.diff
 		TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --enable-only64bit"
 	elif [ "$TERMUX_ARCH" == "arm" ]; then
 		# valgrind doesn't like arm; armv7 works, though.
@@ -34,5 +35,5 @@ termux_step_pre_configure() {
 		termux_setup_no_integrated_as
 	fi
 
-	autoreconf -fi
+	autoreconf -vfi
 }
