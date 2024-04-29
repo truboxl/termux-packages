c1b13074cc5b9887d64ddafb2436ef5e04a4bb91
Jia Yuan Lo <jylo06g@gmail.com>
2024-04-30 04:29:32 +0800

    android

--- packages/outfieldr/build.sh
+++ packages/outfieldr/build.sh
@@ -21,6 +21,8 @@ termux_step_post_get_source() {
 
 termux_step_pre_configure() {
 	termux_setup_zig
+	ZIG_TARGET_NAME=${TERMUX_ARCH}-linux-android
+	[[ "${TERMUX_ARCH}" == "arm" ]] && ZIG_TARGET_NAME=arm-linux-androideabi
 }
 
 termux_step_make() {
