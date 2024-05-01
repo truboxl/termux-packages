04a8df791f5319e1f7e01e2d321cc145ec77f8b7
Jia Yuan Lo <jylo06g@gmail.com>
2024-05-01 12:10:17 +0800

    scripts(lint-packages): add blank end of line check
    Also fix a bug where TERMUX_PKG_SRCURL error message is truncated

--- scripts/lint-packages.sh
+++ scripts/lint-packages.sh
@@ -135,6 +135,16 @@ lint_package() {
 		check_package_name "$subpkg_name" || return 1
 	done
 
+	echo -n "Blank End Of Line check: "
+	local endofline
+	endofline=$(tail -n1 "$package_script")
+	if (( ! ${#endofline} )); then
+		echo -e "FAILED\n"
+		tail -n5 "$package_script" | sed -e "s|^|  |" -e "5s|^  |>>|"
+		return 1
+	fi
+	echo "PASS"
+
 	echo -n "File permission check: "
 	local file_permission
 	file_permission=$(stat -c "%A" "$package_script")
@@ -298,7 +308,6 @@ lint_package() {
 
 		echo -n "TERMUX_PKG_SRCURL: "
 		if (( ${#TERMUX_PKG_SRCURL} )); then
-
 			urls_ok=true
 			for url in "${TERMUX_PKG_SRCURL[@]}"; do
 				if (( ${#url} )); then
@@ -351,9 +360,12 @@ lint_package() {
 				pkg_lint_error=true
 			fi
 		else
+			echo -n "NOT SET"
 			if [[ "$TERMUX_PKG_SKIP_SRC_EXTRACT" != 'true' ]] && ! declare -F termux_step_extract_package > /dev/null 2>&1; then
-				echo "TERMUX_PKG_SRCURL: NOT SET (set TERMUX_PKG_SKIP_SRC_EXTRACT to 'true' if no sources downloaded)"
+				echo " (set TERMUX_PKG_SKIP_SRC_EXTRACT to 'true' if no sources downloaded)"
 				pkg_lint_error=true
+			else
+				echo " (acceptable since TERMUX_PKG_SKIP_SRC_EXTRACT is true)"
 			fi
 		fi
 
