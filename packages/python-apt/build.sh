417d73d49cd33864c50db6366f7b456dcc658866
Jia Yuan Lo <jylo06g@gmail.com>
2024-05-11 18:53:54 +0800

    bump(main/python-apt): 2.9.0

--- packages/python-apt/build.sh
+++ packages/python-apt/build.sh
@@ -2,14 +2,31 @@ TERMUX_PKG_HOMEPAGE=https://apt-team.pages.debian.net/python-apt/
 TERMUX_PKG_DESCRIPTION="Python bindings for APT"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2.7.2"
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION="2.9.0"
 TERMUX_PKG_SRCURL=https://ftp.debian.org/debian/pool/main/p/python-apt/python-apt_${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=00371ae55d7ebc5fea3b4973f440e0f3683afbe1af7e969706a28c60b47a34d9
-TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="apt, build-essential, libc++, python, texinfo"
+TERMUX_PKG_SHA256=5b7bf0b19a798a5ee0b8d027e162282db41b957f6586ffc8b90df827b650feb2
+TERMUX_PKG_DEPENDS="apt, libandroid-support, libc++, python"
 TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_PYTHON_COMMON_DEPS="wheel"
+
+termux_pkg_auto_update() {
+	# based on scripts/updates/api/termux_repology_api_get_latest_version.sh
+	local TERMUX_REPOLOGY_DATA_FILE=$(mktemp)
+	python3 "${TERMUX_SCRIPTDIR}"/scripts/updates/api/dump-repology-data \
+		"${TERMUX_REPOLOGY_DATA_FILE}" "${TERMUX_PKG_NAME}" >/dev/null || \
+		echo "{}" > "${TERMUX_REPOLOGY_DATA_FILE}"
+	local latest_version=$(jq -r --arg packageName "${TERMUX_PKG_NAME}" '.[$packageName]' < "${TERMUX_REPOLOGY_DATA_FILE}")
+	if [[ -z "${latest_version}" ]]; then
+		cat <<- EOL >&2
+		WARN: Auto update failure!
+		$(cat "${TERMUX_REPOLOGY_DATA_FILE}")
+		EOL
+		return
+	fi
+	rm -f "${TERMUX_REPOLOGY_DATA_FILE}"
+	termux_pkg_upgrade_version "${latest_version}"
+}
 
 termux_step_pre_configure() {
 	export DEBVER="${TERMUX_PKG_VERSION#*:}"
