8f1a41f0f7d15861aeb576ac3d855b92ea826c8e
Jia Yuan Lo <jylo06g@gmail.com>
2024-07-19 17:07:31 +0800

    chore(x11/mogan): move around files

--- x11-packages/mogan/mogan-data.subpackage.sh
+++ x11-packages/mogan/mogan-data.subpackage.sh
@@ -1,3 +1,10 @@
 TERMUX_SUBPKG_DESCRIPTION="Platform-independent data for mogan"
 TERMUX_SUBPKG_PLATFORM_INDEPENDENT=true
-TERMUX_SUBPKG_INCLUDE="share/Xmacs/"
+TERMUX_SUBPKG_BREAKS="mogan (<< 1.2.5.5)"
+TERMUX_SUBPKG_REPLACES="mogan (<< 1.2.5.5)"
+TERMUX_SUBPKG_INCLUDE="
+share/Xmacs
+share/applications
+share/icons
+share/mime
+"
