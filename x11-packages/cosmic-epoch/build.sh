d61c3ee41dcfa3503bdb153eb79dde4b66521a37
Jia Yuan Lo <jylo06g@gmail.com>
2024-07-31 23:52:43 +0800

    ce

--- x11-packages/cosmic-epoch/build.sh
+++ x11-packages/cosmic-epoch/build.sh
@@ -19,7 +19,7 @@ termux_step_pre_configure() {
 	echo "${justfiles}" | xargs -P${TERMUX_PKG_MAKE_PROCESSES} -i -r sed \
 		-e "s|/usr/local|${TERMUX_PREFIX}|g" \
 		-e "s|/usr|${TERMUX_PREFIX}|g" \
-		-e "s|cargo build|rustup target add ${CARGO_TARGET_NAME}; rm -fr ${CARGO_HOME}/registry/src/*/rustix-*; cargo fetch --target ${CARGO_TARGET_NAME}; for a in ${CARGO_HOME}/registry/src/*/rustix-*; do patch -p1 -i ${TERMUX_PKG_BUILDER_DIR}/rustix.patch -d \$a; done; cargo build --target ${CARGO_TARGET_NAME}; ln -fsv target/${CARGO_TARGET_NAME}/release target/release|g" \
+		-e "s|cargo build|rustup target add ${CARGO_TARGET_NAME}; rm -fr ${CARGO_HOME}/registry/src/*/rustix-*; cargo fetch --target ${CARGO_TARGET_NAME}; for a in ${CARGO_HOME}/registry/src/*/rustix-*; do patch -p1 -i ${TERMUX_PKG_BUILDER_DIR}/rustix.diff -d \$a; done; cargo build --target ${CARGO_TARGET_NAME}; ln -fsv target/${CARGO_TARGET_NAME}/release target/release|g" \
 		-i "{}"
 }
 
