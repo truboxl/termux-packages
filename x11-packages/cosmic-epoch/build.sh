70c5876d2b27ecc52619bf75b3fdbe33167a053a
Jia Yuan Lo <jylo06g@gmail.com>
2024-07-31 21:30:53 +0800

    ce

--- x11-packages/cosmic-epoch/build.sh
+++ x11-packages/cosmic-epoch/build.sh
@@ -7,7 +7,6 @@ TERMUX_PKG_SRCURL=git+https://github.com/pop-os/cosmic-epoch
 TERMUX_PKG_GIT_BRANCH=master
 TERMUX_PKG_DEPENDS="libxkbcommon"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_RUST_VERSION="1.75.0"
 
 termux_step_pre_configure() {
 	termux_setup_just
@@ -17,7 +16,7 @@ termux_step_pre_configure() {
 	echo "${justfiles}" | tee | xargs -P${TERMUX_PKG_MAKE_PROCESSES} -i -r sed \
 		-e "s|/usr/local|${TERMUX_PREFIX}|g" \
 		-e "s|/usr|${TERMUX_PREFIX}|g" \
-		-e "s|cargo build|cargo build --target ${CARGO_TARGET_NAME}|g" \
+		-e "s|cargo build|command -v cargo; rustup target add ${CARGO_TARGET_NAME}; cargo build --target ${CARGO_TARGET_NAME}|g" \
 		-i "{}"
 }
 
