d8ae5ccd2803d550e1b15b143239734517301ec0
Jia Yuan Lo <jylo06g@gmail.com>
2024-08-11 15:05:47 +0800

    addpkg(main): libudev-zero

---
+++ packages/libudev-zero/build.sh
@@ -0,0 +1,13 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/illiliti/libudev-zero
+TERMUX_PKG_DESCRIPTION="Daemonless replacement for libudev"
+TERMUX_PKG_LICENSE="ISC"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="1.0.3"
+TERMUX_PKG_SRCURL=https://github.com/illiliti/libudev-zero/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=0bd89b657d62d019598e6c7ed726ff8fed80e8ba092a83b484d66afb80b77da5
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_EXTRA_MAKE_ARGS="
+AR=llvm-ar
+PREFIX=${TERMUX_PREFIX}
+"
