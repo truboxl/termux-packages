41d9c8baa5ce0f936a902e88a9166f8f7477e7c0
Jia Yuan Lo <jylo06g@gmail.com>
2024-08-17 19:52:06 +0800

    source

--- packages/cpio/build.sh
+++ packages/cpio/build.sh
@@ -9,18 +9,12 @@ TERMUX_PKG_SHA256=937610b97c329a1ec9268553fb780037bcfff0dcffe9725ebc4fd9c1aa9075
 TERMUX_PKG_DEPENDS="tar"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-rmt=$TERMUX_PREFIX/libexec/rmt"
 
-termux_step_post_get_source() {
-	termux_download https://android.googlesource.com/platform/bionic/+archive/refs/heads/main/libc/tzcode.tar.gz tzcode.tar.gz SKIP_CHECKSUM
-	mkdir -p ${TERMUX_PKG_SRCDIR}/tzcode
-	tar -xf tzcode.tar.gz -C ${TERMUX_PKG_SRCDIR}/tzcode
-}
-
 termux_step_pre_configure() {
-	pushd ${TERMUX_PKG_SRCDIR}/tzcode
-	${CC} localtime.c -c
+	pushd ${TERMUX_PKG_TMPDIR}
+	${CC} ${TERMUX_PKG_BUILDER_DIR}/localtime.c -c
 	${AR} rcu libtzcode.a *.o
 	popd
-	LDFLAGS+=" -L${TERMUX_PKG_SRCDIR}/tzcode -l:libtzcode.a"
+	LDFLAGS+=" -L${TERMUX_PKG_TMPDIR} -l:libtzcode.a"
 
 	autoreconf -fi
 }
