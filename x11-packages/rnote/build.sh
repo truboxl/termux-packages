ee403b9acddb432154517d4fde64abcb0c6cafcb
Jia Yuan Lo <jylo06g@gmail.com>
2024-08-22 19:57:19 +0800

    addpkg(x11): rnote

---
+++ x11-packages/rnote/build.sh
@@ -0,0 +1,28 @@
+TERMUX_PKG_HOMEPAGE=https://rnote.flxzt.net/
+TERMUX_PKG_DESCRIPTION="Open-source vector-based drawing app"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="0.11.0"
+TERMUX_PKG_SRCURL=https://github.com/flxzt/rnote/releases/download/v${TERMUX_PKG_VERSION}/rnote-${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_SHA256=567d30523ef672a18ca33b16442af7b4a93910b47f416d09968dbc0e1d689cc3
+TERMUX_PKG_DEPENDS="appstream, glib, gtk4, libadwaita, libcairo, poppler"
+TERMUX_PKG_AUTO_UPDATE=true
+
+termux_step_pre_configure() {
+	termux_setup_cmake
+	termux_setup_rust
+
+	export CFLAGS=""
+	export GETTEXT_SYSTEM=1
+
+	# remove rodio -> cpal -> alsa
+	# dependencies that cannot be built
+	pushd crates/rnote-engine
+	cargo remove rodio
+	popd
+
+	local p="${TERMUX_PKG_BUILDER_DIR}/0001-meson.build.diff"
+	echo "Applying patch: $(basename "${p}")"
+	sed "s|@CARGO_TARGET_NAME@|${CARGO_TARGET_NAME}|" "${p}" \
+		| patch --silent -p1
+}
