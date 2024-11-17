9f18785760282868b0f6c0ddcb05d4e8aa77152c
Jia Yuan Lo <jylo06g@gmail.com>
2024-11-17 21:40:00 +0800

    Build bootstrap packages
    Exclude proot: missing support
    Exclude command-not-found: missing Contents-riscv64.gz on server
    Exclude debianutils: builds command-not-found

--- packages/diffutils/build.sh
+++ packages/diffutils/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Programs (cmp, diff, diff3 and sdiff) related to finding
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="3.11"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/diffutils/diffutils-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=a73ef05fe37dd585f7d87068e4a0639760419f810138bd75c61ddaa1f9e2131e
 TERMUX_PKG_DEPENDS="libiconv"
