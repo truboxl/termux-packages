0ab6585aa537e2ee54e237d36911273be541ba5e
Jia Yuan Lo <jylo06g@gmail.com>
2025-01-22 20:23:39 +0800

    f

--- packages/libclc/build.sh
+++ packages/libclc/build.sh
@@ -5,4 +5,4 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="19.1.7"
 TERMUX_PKG_SRCURL=https://github.com/llvm/llvm-project/releases/download/llvmorg-${TERMUX_PKG_VERSION}/libclc-${TERMUX_PKG_VERSION}.src.tar.xz
 TERMUX_PKG_SHA256=77e2d71f5cea1d0b1014ba88186299d1a0848eb3dc20948baae649db9e7641cb
-TERMUX_PKG_DEPENDS="libc++"
+TERMUX_PKG_DEPENDS="libc++, libllvm"
