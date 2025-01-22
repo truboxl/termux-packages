0c694b45c9422b574724bc5904684bdf2d2012b4
Jia Yuan Lo <jylo06g@gmail.com>
2025-01-22 19:16:19 +0800

    addpkg(main): libclc

---
+++ packages/libclc/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://libclc.llvm.org
+TERMUX_PKG_DESCRIPTION="Open source implementation of OpenCL 1.1 library"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="19.1.7"
+TERMUX_PKG_SRCURL=https://github.com/llvm/llvm-project/releases/download/llvmorg-${TERMUX_PKG_VERSION}/libclc-${TERMUX_PKG_VERSION}.src.tar.xz
+TERMUX_PKG_SHA256=77e2d71f5cea1d0b1014ba88186299d1a0848eb3dc20948baae649db9e7641cb
+TERMUX_PKG_DEPENDS="libc++"
