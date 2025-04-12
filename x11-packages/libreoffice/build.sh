d3008d3b981a283d45da8d76d0a17785069789b7
Jia Yuan Lo <jylo06g@gmail.com>
2025-04-12 22:51:11 +0800

    zlib

--- x11-packages/libreoffice/build.sh
+++ x11-packages/libreoffice/build.sh
@@ -6,13 +6,13 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="25.2.2.2"
 UTERMUX_PKG_SRCURL=git+https://github.com/LibreOffice/core
 UTERMUX_PKG_GIT_BRANCH=master
-TERMUX_PKG_SRCURL=(
+UTERMUX_PKG_SRCURL=(
 	https://download.documentfoundation.org/libreoffice/src/${TERMUX_PKG_VERSION%.*}/libreoffice-${TERMUX_PKG_VERSION}.tar.xz
 	https://download.documentfoundation.org/libreoffice/src/${TERMUX_PKG_VERSION%.*}/libreoffice-dictionaries-${TERMUX_PKG_VERSION}.tar.xz
 	https://download.documentfoundation.org/libreoffice/src/${TERMUX_PKG_VERSION%.*}/libreoffice-help-${TERMUX_PKG_VERSION}.tar.xz
 	https://download.documentfoundation.org/libreoffice/src/${TERMUX_PKG_VERSION%.*}/libreoffice-translations-${TERMUX_PKG_VERSION}.tar.xz
 )
-TERMUX_PKG_SHA256=(
+UTERMUX_PKG_SHA256=(
 	01a14580c15a5b14153fa46c28e90307f6683e0d0326727a4ad13e9545dfe6ac
 	18460f0ae1140b2faed8e513e83e796fb86570f5ab8a727679ccb0c1ed9f7838
 	5a7c9fcc43afad674c3883f90c46f5e4fde728801a0d19e37e518bdc488a5f9e
@@ -20,17 +20,24 @@ TERMUX_PKG_SHA256=(
 )
 TERMUX_PKG_SRCURL=https://download.documentfoundation.org/libreoffice/src/${TERMUX_PKG_VERSION%.*}/libreoffice-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=01a14580c15a5b14153fa46c28e90307f6683e0d0326727a4ad13e9545dfe6ac
-TERMUX_PKG_DEPENDS="fontconfig, libc++"
+TERMUX_PKG_DEPENDS="fontconfig, libc++, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
-"
 
-termux_step_pre_configure() {
-	if [[ "${TERMUX_ON_DEVICE_BUILD}" == "false" ]]; then
-		TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --build x86_64-linux-gnu"
-	fi
-	TERMUX_PKG_EXTRA_CONFIGURE_ARGS+="
-	--host ${TERMUX_ARCH}-linux-gnu
-	--with-jdk-home ${TERMUX_JAVA_HOME}
-	"
+termux_step_configure() {
+	# TERMUX_PKG_EXTRA_CONFIGURE_ARGS breaks when need to pass quoted variables
+	./configure \
+		--prefix=${TERMUX_PREFIX} \
+		--libdir=${TERMUX_PREFIX}/lib \
+		--sbindir=${TERMUX_PREFIX}/bin \
+		--host=${TERMUX_ARCH}-linux-gnu \
+		--with-jdk-home=${TERMUX_JAVA_HOME} \
+		ac_cv_lib_z_deflate=yes \
+		|| termux_step_configure_autotools_failure_hook
+}
+
+termux_step_configure_autotools_failure_hook() {
+	echo "########## LOG ##########"
+	ls -l CONF-FOR-BUILD
+	cat CONF-FOR-BUILD/config.log
+	exit 1
 }
