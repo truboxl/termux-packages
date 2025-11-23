0bd6a7b03fa316501d3bd1b7b86f0f2fbae9532d
Jia Yuan Lo <jylo06g@gmail.com>
2025-11-23 23:11:54 +0800

    addpkg(root): colima

---
+++ root-packages/colima/build.sh
@@ -0,0 +1,18 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/abiosoft/colima
+TERMUX_PKG_DESCRIPTION="Container runtimes on macOS (and Linux) with minimal setup"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="0.9.1"
+TERMUX_PKG_SRCURL=git+https://github.com/abiosoft/colima
+TERMUX_PKG_GIT_BRANCH="v${TERMUX_PKG_VERSION}"
+TERMUX_PKG_DEPENDS="curl, lima"
+TERMUX_PKG_ANTI_BUILD_DEPENDS="curl, lima"
+TERMUX_PKG_SUGGESTS="docker"
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_EXCLUDED_ARCHES="arm, i686"
+
+termux_step_pre_configure() {
+	termux_setup_golang
+
+	TERMUX_PKG_EXTRA_MAKE_ARGS+="OS=android ARCH=${TERMUX_ARCH} INSTALL_DIR=${TERMUX_PREFIX}/bin"
+}
---
+++ root-packages/colima/tests/0001-colima-start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+pkg install -y root-packages
+pkg install -y docker
+#pkg install -y colima
+
+command -v colima
+limactl -V
+command -v docker
+docker -V
+
+tmpdir=$(mktemp -d)
+pushd "$tmpdir"
+
+colima start
+docker run hello-world
+docker stop
+colima stop
+
+popd
+rm -fr "$tmpdir"
