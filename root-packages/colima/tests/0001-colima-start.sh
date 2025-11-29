1c17359ae4f871b66ad62b5ef684ae02950d67af
Jia Yuan Lo <jylo06g@gmail.com>
2025-11-30 01:12:35 +0800

    colima

--- root-packages/colima/tests/0001-colima-start.sh
+++ root-packages/colima/tests/0001-colima-start.sh
@@ -5,12 +5,12 @@ pkg install -y root-repo
 pkg install -y docker
 #pkg install -y colima
 
-command -v colima || echo "colima is not installed" && exit 1
-colima version
-command -v docker || echo "docker is not installed" && exit 1
-docker --version
-command -v limactl || echo "limactl is not installed" && exit 1
-limactl --version
+#command -v colima || echo "colima is not installed" && exit 1
+#colima version
+#command -v docker || echo "docker is not installed" && exit 1
+#docker --version
+#command -v limactl || echo "limactl is not installed" && exit 1
+#limactl --version
 
 tmpdir=$(mktemp -d)
 pushd "$tmpdir"
