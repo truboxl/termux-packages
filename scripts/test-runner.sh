3ea5c03546522204168a2afd7f15b8b8396779b1
Jia Yuan Lo <jylo06g@gmail.com>
2025-11-29 20:39:55 +0800

    scripts: allow test-runner.sh parse multiple packages

--- scripts/test-runner.sh
+++ scripts/test-runner.sh
@@ -1,42 +1,53 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-if [ $# != 1 ]; then
-	echo "Specify package to run tests for as only argument"
+if [[ "$#" == 0 ]]; then
+	echo "Specify package(s) as argument to run tests"
 	exit 1
 fi
 
-PACKAGE=$1
-TEST_DIR=packages/$PACKAGE/tests
+PACKAGES=($@)
 
-if [ ! -d $TEST_DIR ]; then
-	echo "ERROR: No tests folder for package $PACKAGE"
-	exit 1
-fi
+for (( i=0; i < ${#PACKAGES[@]}; i++ )); do
 
-NUM_TESTS=0
-NUM_FAILURES=0
+	PACKAGE="${PACKAGES[$i]}"
+	TEST_DIR="$(find . -mindepth 2 -maxdepth 2 -name $PACKAGE -type d)/tests"
 
-for TEST_SCRIPT in $TEST_DIR/*; do
-	test -t 1 && printf "\033[32m"
-	echo "Running test ${TEST_SCRIPT}..."
-	(( NUM_TESTS += 1 ))
-	test -t 1 && printf "\033[31m"
-	(
-		assert_equals() {
-			FIRST=$1
-			SECOND=$2
-			if [ "$FIRST" != "$SECOND" ]; then
-				echo "assertion failed - expected '$FIRST', got '$SECOND'"
-				exit 1
-			fi
-		}
-		set -e -u
-		. $TEST_SCRIPT
-	)
-	if [ $? != 0 ]; then
-		(( NUM_FAILURES += 1 ))
+	if [[ ! -d "$TEST_DIR" ]]; then
+		echo "WARN: No tests folder for package $PACKAGE, skipping"
+		continue
 	fi
-	test -t 1 && printf "\033[0m"
+
+	NUM_TESTS=0
+	NUM_FAILURES=0
+
+	for TEST_SCRIPT in $TEST_DIR/*; do
+		test -t 1 && printf "\033[32m"
+		echo "Running test ${TEST_SCRIPT}..."
+		(( NUM_TESTS += 1 ))
+		test -t 1 && printf "\033[31m"
+		(
+			assert_equals() {
+				FIRST=$1
+				SECOND=$2
+				if [ "$FIRST" != "$SECOND" ]; then
+					echo "assertion failed - expected '$FIRST', got '$SECOND'"
+					exit 1
+				fi
+			}
+			set -e -u
+			. $TEST_SCRIPT
+		)
+		result="$?"
+		echo "test exited $result"
+		if [[ "$result" != 0 ]]; then
+			(( NUM_FAILURES += 1 ))
+		fi
+		test -t 1 && printf "\033[0m"
+	done
+
+	echo "$PACKAGE: $NUM_TESTS tests run - $NUM_FAILURES failure(s)"
 done
 
-echo "$NUM_TESTS tests run - $NUM_FAILURES failure(s)"
+if [[ "$NUM_FAILURES" != 0 ]]; then
+	exit 1
+fi
